75a9d280651640de53220b4c047034f65f6f7967
Tubolayefa <tlayefaah@gmail.com>
2024-09-04 15:39:35 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const level2 = document.querySelector(".speed-2");
+const level3 = document.querySelector(".speed-3");
+
 const buttons = {
   left: document.querySelector(".left"),
   right: document.querySelector(".right"),
   reverse: document.querySelector(".reverse"),
   forward: document.querySelector(".forward"),
+  speedUp: document.querySelector(".speed-up"),
+  speedDown: document.querySelector(".speed-down"),
 };
 
 const states = {
@@ -16,6 +21,9 @@ const states = {
 
 var gateway = "ws://192.168.1.184/ws";
 var websocket;
+
+let activeTouches = {}; // Initialize speed level
+let speed = 1; // Initialize speed level
 
 window.addEventListener("load", initWebSocket);
 
@@ -33,7 +41,18 @@ function changeState(state) {
   websocket.send(state);
 }
 
-let activeTouches = {};
+function speedBar() {
+  if (speed === 2) {
+    level2.style.backgroundColor = "#00700f";
+    level3.style.backgroundColor = "white";
+  } else if (speed === 3) {
+    level3.style.backgroundColor = "#004e0b";
+    level2.style.backgroundColor = "#00700f";
+  } else {
+    level2.style.backgroundColor = "white";
+    level3.style.backgroundColor = "white";
+  }
+}
 
 function handleTouchStart(event, button) {
   const touchId = event.changedTouches[0].identifier;
@@ -42,7 +61,21 @@ function handleTouchStart(event, button) {
     () => console.log(`${button} button is being held`),
     500
   );
-  changeState(states[button]);
+
+  if (button === "speedUp" && speed < 3) {
+    speed++;
+    speedBar();
+    changeState(`speed${speed}`);
+    console.log(`speed${speed}`);
+  } else if (button === "speedDown" && speed > 1) {
+    speed--;
+    speedBar();
+    console.log(`speed${speed}`);
+    changeState(`speed${speed}`);
+} else {
+      console.log(`speed${speed}`);
+    changeState(states[button]);
+  }
 }
 
 function handleTouchEnd(event, button) {
@@ -52,14 +85,12 @@ function handleTouchEnd(event, button) {
     delete activeTouches[touchId];
   }
   event.target.style.color = "";
-  
-  if(button === 'forward' || button=== 'reverse'){
 
-      changeState(states.off);
-    } else if(button === 'right' || button === 'left'){
-
-        changeState(states.straight);
-    }
+  if (button === "forward" || button === "reverse") {
+    changeState(states.off);
+  } else if (button === "right" || button === "left") {
+    changeState(states.straight);
+  }
 }
 
 Object.keys(buttons).forEach((button) => {
